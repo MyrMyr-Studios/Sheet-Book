5f1ebace52c07405070f28c306ca8e5f7f34a1ec
Diogo <diogoribfaria@usp.br>
2024-05-27 00:01:04 -0300

    Configurado o projeto de frontend. Utilizando Vite e React com TypeScript e TailwindCSS. Atualizado o .gitignore para ignorar arquivos de configuração do Vite e do Node.

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline mb-10">Hello Vite + React!</h1>
+      <button className="btn btn-primary">Hello Daisy button</button>
+    </>
+  )
+}
+
+export default App
---
+++ web/tailwind.config.js
@@ -0,0 +1,15 @@
+import { plugin } from 'postcss'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
+
+module.exports = {
+  content: ["node_modules/daisyui/dist/**/*.js", "node_modules/react-daisyui/dist/**/*.js"],
+  plugins: [require("daisyui")],
+}
