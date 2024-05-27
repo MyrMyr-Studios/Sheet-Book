fa6ef5f9e8045304d832cd174ae6361817c8ff42
Diogo <diogoribfaria@usp.br>
2024-05-27 00:55:41 -0300

    Atualizados Makefile e docker-compose para incluir a construção da imagem do frontend.

--- Makefile
+++ Makefile
@@ -1,4 +1,13 @@
+.PHONY: all frontend backend down clean
+
 all:
+	docker compose up app --build --detach
+	docker compose up web --detach
+
+frontend:
+	docker compose up web --detach
+
+backend:
 	docker compose up app --build --detach
 
 down:
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,8 +4,8 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline mb-10">Hello Vite + React!</h1>
-      <button className="btn btn-primary">Hello Daisy button</button>
+      <h1 className="text-3xl font-bold underline mb-10">Sheet Book</h1>
+      <button className="btn btn-primary">Fetch data</button>
     </>
   )
 }
