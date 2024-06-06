51ab6f7bd655b6de6d91c653c426eb6244f003ed
Diogo <diogoribfaria@gmail.com>
2024-06-05 21:50:25 -0300

    Finalizados forms de login e registro

--- web/src/pages/home.tsx
+++ web/src/pages/home.tsx
@@ -5,24 +5,22 @@ import { themeChange } from 'theme-change';
 import { Link } from 'react-router-dom';
 
 
-const Home = () => {
+function Home() {
+  const [username, setUsername] = useState<string | null>(null)
+  const [password, setPassword] = useState<string | null>(null)
+  const [email, setEmail] = useState<string | null>(null)
+
   useEffect(() => {
     themeChange(false);
     getLoggedInUser();
   }, []);
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [username, setUsername] = useState('')
+
 
   const getLoggedInUser = () => {
     axios
       .get('/user')
       .then((response) => {
-        if (response.status === 200) {
-          setUsername(response.data)
-        }
-      }, (error) => {
-        console.log(error.request.response)
+        if (response.status === 200) setUsername(response.data)
       })
   }
 
@@ -30,13 +28,89 @@ const Home = () => {
     axios
       .get('/login', {params: {email: email, password: password}})
       .then((response) => {
-        if (response.status === 200) {
-          getLoggedInUser();
+        if (response.status === 200) getLoggedInUser();
+      }).catch((error) => {
+        if (error.response.status === 404) {
+          const emailInput = document.getElementById("email")
+          emailInput?.classList.add("input-error")
         }
-      }, (error) => {
-        console.log(error.request.response)
+        else if (error.response.status === 401) {
+          const passwordInput = document.getElementById("password")
+          passwordInput?.classList.add("input-error")
+        }
       })
-    }
+  }
+  
+  const loggout = () => {
+    axios
+      .get('/logout')
+      .then((response) => {
+        if (response.status === 200) setUsername(null)
+      })
+  }
+
+  const validateLogin = () => {
+    const emailInput = document.getElementById("email")
+    const passwordInput = document.getElementById("password")
+    if (!email || email == "")
+      emailInput?.classList.add("input-error")
+    else
+      emailInput?.classList.remove("input-error")
+
+    if (!password || password == "")
+      passwordInput?.classList.add("input-error")
+    else
+      passwordInput?.classList.remove("input-error")
+
+    if ((email && password) && (email != "" && password != ""))
+      login();
+  }
+  
+  const not_logged_dropdown = (
+    <div className="flex-none">
+      <span className="text-primary-content">Login</span>
+      <Dropdown end>
+        <Button tag="label" tabIndex={0} color="ghost" shape="circle">
+          <span className="material-icons text-primary-content" style={{lineHeight: "1rem"}}>expand_more</span>
+        </Button>
+        <Dropdown.Menu className="mt-3 z-[1] w-52 menu-sm">
+          <Input type="text" placeholder="Email" className="input input-bordered" style={{marginBottom: "0.5rem", width: "15rem"}} onChange={(e) => setEmail(e.target.value)} id='email'/>
+          <Input type="password" placeholder="Password" className="input input-bordered" style={{marginBottom: "0.5rem", width: "15rem"}} onChange={(e) => setPassword(e.target.value)} id='password'/>
+          <Button className="btn btn-primary" style={{width: "15rem", marginBottom: "0.5rem"}} onClick={validateLogin}>Login</Button>
+          <Link to="/register">
+            <Button className="btn btn-secondary" style={{width: "15rem"}}>Register</Button>
+          </Link>
+        </Dropdown.Menu>
+      </Dropdown>
+    </div>
+  )
+
+  const logged_dropdown = (
+    <div className="flex-none">
+      <span className="text-primary-content">{username}</span>
+      <Dropdown end>
+        <Button tag="label" tabIndex={0} color="ghost" shape="circle">
+          <span className="material-icons text-primary-content" style={{lineHeight: "1rem"}}>expand_more</span>
+        </Button>
+        <Dropdown.Menu className="mt-3 z-[1] w-52 menu-sm">
+          {/* <div className="flex items-center justify-center gap-2">
+            <Button className="btn btn-primary" style={{width: "7rem"}} onClick={login}>Login</Button>
+            <Button className="btn btn-secondary" style={{width: "7rem"}} onClick={register}>Register</Button>
+          </div> */}
+
+          {/* <hr style={{marginTop: ".5rem", marginBottom: ".5rem"}}/> */}
+          <li>
+            <a className="justify-between">
+              Profile
+              <Badge className="badge">New</Badge>
+            </a>
+          </li>
+          <Dropdown.Item>Settings</Dropdown.Item>
+          <Dropdown.Item onClick={loggout}>Logout</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </div>
+  )
 
   return (
     <>
@@ -45,38 +119,7 @@ const Home = () => {
           <img style={{height: "3rem"}} src="/sheet-book-white.png" />
           <span className="text-primary-content" style={{paddingLeft: "1rem", fontWeight: "700", fontSize: "1.55rem"}}>Sheet Book</span>
         </div>
-        <div className="flex-none">
-          <span className="text-primary-content" style={{paddingRight: ".5rem"}}>{username}</span>
-          <Dropdown end>
-            <Button tag="label" tabIndex={0} color="ghost" className="avatar" shape="circle">
-              <div className="w-10 rounded-full">
-                <img src="/sheet-book-outline-blue.png" />
-              </div>
-            </Button>
-            <Dropdown.Menu className="mt-3 z-[1] w-52 menu-sm">
-              <Input type="text" placeholder="Email" className="input input-bordered" style={{marginBottom: "0.5rem", width: "15rem"}} onChange={(e) => setEmail(e.target.value)} />
-              <Input type="password" placeholder="Password" className="input input-bordered" style={{marginBottom: "0.5rem", width: "15rem"}} onChange={(e) => setPassword(e.target.value)} />
-              <Button className="btn btn-primary" style={{width: "15rem", marginBottom: "0.5rem"}} onClick={login}>Login</Button>
-              <Link to="/register">
-                <Button className="btn btn-secondary" style={{width: "15rem"}}>Register</Button>
-              </Link>
-              {/* <div className="flex items-center justify-center gap-2">
-                <Button className="btn btn-primary" style={{width: "7rem"}} onClick={login}>Login</Button>
-                <Button className="btn btn-secondary" style={{width: "7rem"}} onClick={register}>Register</Button>
-              </div> */}
-
-              {/* <hr style={{marginTop: ".5rem", marginBottom: ".5rem"}}/>
-              <li>
-                <a className="justify-between">
-                  Profile
-                  <Badge className="badge">New</Badge>
-                </a>
-              </li>
-              <Dropdown.Item>Settings</Dropdown.Item>
-              <Dropdown.Item>Logout</Dropdown.Item> */}
-            </Dropdown.Menu>
-          </Dropdown>
-        </div>
+        {username ? logged_dropdown : not_logged_dropdown}
       </Navbar>
     </>
   )
--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -2,38 +2,103 @@ import axios from 'axios'
 import {useState, useEffect} from "react";
 import {Input, Button} from 'react-daisyui'
 import { themeChange } from 'theme-change';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 
-const Register = () => {
+function Register() {
   useEffect(() => {
     themeChange(false);
   }, []);
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState<string | null>(null)
+  const [password, setPassword] = useState<string | null>(null)
+  const [username, setUsername] = useState<string | null>(null)
+  const navigate = useNavigate()
 
   const register = () => {
     axios
       .post('/login', {name: username, email: email, password: password})
       .then((response) => {
-        if (response.status === 200) {
-          console.log(response.data)
+        if(response.status === 201) navigate('/')
+      }).catch((error) => {
+        if(error.response.status === 409) {
+          const emailInput = document.getElementById("email")
+          emailInput?.classList.add("input-error")
+          const email_error = document.getElementById("email_error")
+          if(email_error) email_error.innerText = "Email already in use"
         }
-      }, (error) => {
-        console.log(error.request.response)
       })
   }
 
+  const validateUsername = (username: string) => {
+    const usernameInput = document.getElementById("username")
+    const username_error = document.getElementById("username_error")
+    if(username === "") {
+      usernameInput?.classList.add("input-error")
+      if(username_error) username_error.innerText = "Username cannot be empty"
+    }
+    else {
+      usernameInput?.classList.remove("input-error")
+      if(username_error) username_error.innerText = ""
+    }
+    setUsername(username)
+  }
+
+  const validateEmail = (email: string) => {
+    const emailInput = document.getElementById("email")
+    const email_error = document.getElementById("email_error")
+    if(email === "") {
+      emailInput?.classList.add("input-error")
+      if(email_error) email_error.innerText = "Email cannot be empty"
+    }
+    else {
+      emailInput?.classList.remove("input-error")
+      if(email_error) email_error.innerText = ""
+    }
+    setEmail(email)
+  }
+
+  const validatePassword = (password: string) => {
+    const passwordInput = document.getElementById("password")
+    const password_error = document.getElementById("password_error")
+    if(password === "") {
+      passwordInput?.classList.add("input-error")
+      if(password_error) password_error.innerText = "Password cannot be empty"
+    }
+    else {
+      passwordInput?.classList.remove("input-error")
+      if(password_error) password_error.innerText = ""
+    }
+    setPassword(password)
+  }
+
+  const validateUserInput = () => {
+    validateEmail(email ? email : "")
+    validatePassword(password ? password : "")
+    validateUsername(username ? username : "")
+    if(email && password && username)
+      if(email !== "" && password !== "" && username !== "")
+        register()
+  }
+
+
   return (
     <>
-    <div className="flex gap-2" style={{flexDirection: "column", alignItems: "center", position: "absolute", top: "40%", left: "50%", transform: "translate(-50%, -50%)"}}>
-        <h1 className="mb-10" style={{fontWeight: "700", fontSize: "2rem", lineHeight:"2.5rem"}}>Sheet Book</h1>
-        <Input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)}/>
-        <Input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
-        <Input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
-        <Button className="btn btn-primary" style={{width: "15rem"}} onClick={register}>Register</Button>
-        <span className="text-primary">Already have an account? <Link to="/" className="text-secondary">Login</Link></span>
+    <div className="" style={{height: "100vh", width: "100vw", position: "absolute"}}></div>
+    <div className="flex gap-2" style={{flexDirection: "column", position: "absolute", top: "40%", left: "50%", transform: "translate(-50%, -50%)"}}>
+        <img style={{height: "4.5rem", width: "4.5rem", alignSelf: "center"}} src="/sheet-book-outline-blue.png" />
+        <div className="flex gap-2 bg-secondary" style={{flexDirection: "column", height: "30rem", width: "40rem", borderRadius: "1rem", padding: "3rem"}}>
+            <label className="label text-secondary-content" style={{fontWeight: "700", lineHeight: "0.1rem"}}>Username</label>
+            <Input type="text" onChange={(e) => validateUsername(e.target.value)} id='username'/>
+            <label className="label text-error" style={{fontSize: "0.75rem", lineHeight: "0.1rem"}} id='username_error'></label>
+            <label className="label text-secondary-content" style={{fontWeight: "700", lineHeight: "0.1rem"}}>Email</label>
+            <Input type="email" onChange={(e) => validateEmail(e.target.value)} id='email' />
+            <label className="label text-error" style={{fontSize: "0.75rem", lineHeight: "0.1rem"}} id='email_error'></label>
+            <label className="label text-secondary-content" style={{fontWeight: "700", lineHeight: "0.1rem"}}>Password</label>
+            <Input type="password" onChange={(e) => validatePassword(e.target.value)} id='password'/>
+            <label className="label text-error" style={{fontSize: "0.75rem", lineHeight: "0.1rem"}} id='password_error'></label>
+            <Button className="btn btn-accent" style={{width: "50%", alignSelf: "center"}} onClick={validateUserInput}>Register</Button>
+            <span className="text-secondary-content" style={{alignSelf: "center"}}>Already have an account? <Link to="/" className="text-accent">Login</Link></span>
+        </div>
     </div>
     </>
   )
