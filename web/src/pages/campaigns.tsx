f3cf41b685f17dd73097cd440a764b3e48dffb03
Diogo <diogoribfaria@usp.br>
2024-06-23 21:55:32 -0300

    Criada pagina de campanhas

--- web/src/pages/campaigns.tsx
+++ web/src/pages/campaigns.tsx
@@ -1,9 +1,37 @@
+import axios from 'axios'
+import {useState, useEffect} from "react";
+import { Link } from 'react-router-dom';
+
 function Campaigns() {
+  const [campaignList, setCampaignList] = useState<any[]>([]);
+
+  useEffect(() => {
+    getCampaignList();
+  }, []);
+
+  const getCampaignList = () => {
+    axios
+      .get('/campaigns')
+      .then((response) => {
+        if (response.status === 200) setCampaignList(response.data)
+      })
+  }
+
   return (
     <div>
-      <h1>Campaigns</h1>
+      <div>
+        {campaignList.map((campaign) => {
+          return (
+            <Link to="/sheets/edit" state={campaign} key={campaign.campaignId}>
+              <div className="bg-secondary" style={{borderRadius: "1rem", padding: "1rem", margin: "1rem"}}>
+                <span className='text-secondary-content'>{campaign.name}</span>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
     </div>
-    );
+  );
 }
 
 export default Campaigns;
